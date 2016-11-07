821269bf319ac43d0eff0dbeed7a1b6e25787c32
Troy Whiteley <troy@somany.us>
2016-11-07 10:38:06 -0800

    Fix asset helper

--- helpers/cdn.js
+++ helpers/cdn.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const path = require('path');
-
 module.exports = (server, asset, done) => {
   const settings = server.settings.app;
 
   const cdn = settings.assets.cdn || '';
 
-  return done(null, `${settings.assets.cdn}/${asset}`);
+  return done(null, `${cdn}${asset}`);
 };
