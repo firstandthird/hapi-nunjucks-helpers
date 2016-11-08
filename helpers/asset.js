ddf288581dedcdd608e0f7b62b74c1ddc6d3b9d0
Troy Whiteley <troy@somany.us>
2016-11-07 17:26:45 -0800

    Support for dist path

--- helpers/asset.js
+++ helpers/asset.js
@@ -5,10 +5,12 @@ const path = require('path');
 module.exports = (server, asset, done) => {
   const settings = server.settings.app;
 
+  const dist = settings.assets.dist || '';
+
   try {
     const mapping = require(path.resolve(process.cwd(), settings.assets.path, 'assets.json'));
-    return done(null, `${settings.assets.endpoint}/${mapping[asset]}`);
+    return done(null, `${settings.assets.endpoint}/${dist}${mapping[asset]}`);
   } catch (e) {
-    return done(null, `${settings.assets.endpoint}/${asset}`);
+    return done(null, `${settings.assets.endpoint}/${dist}${asset}`);
   }
 };
