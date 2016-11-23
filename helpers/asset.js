72cc89ff9cdcf5c12b81d6b55d35c810ed394903
Troy Whiteley <troy@somany.us>
2016-11-23 12:21:51 -0800

    Use readFile and allows asset files to be configured

--- helpers/asset.js
+++ helpers/asset.js
@@ -1,16 +1,31 @@
 'use strict';
 
-const path = require('path');
+const fs = require('fs');
 
 module.exports = (server, asset, done) => {
   const settings = server.settings.app;
-
   const dist = settings.assets.dist || '';
+  const mappingFile = settings.assets.mapping;
 
-  try {
-    const mapping = require(path.resolve(process.cwd(), settings.assets.path, 'assets.json'));
-    return done(null, `${settings.assets.endpoint}/${dist}${mapping[asset]}`);
-  } catch (e) {
-    return done(null, `${settings.assets.endpoint}/${dist}${asset}`);
+  const defaultFile = () => done(null, `${settings.assets.endpoint}/${dist}${asset}`);
+
+  if (!mappingFile) {
+    return defaultFile();
   }
+
+  fs.access(mappingFile, fs.constants.R_OK, err => {
+    if (err) {
+      server.log(['nunjucks-helpers', 'assets'], err);
+      return defaultFile();
+    }
+
+    fs.readFile(mappingFile, (err2, data) => {
+      if (err2) {
+        server.log(['nunjucks-helpers', 'assets'], err2);
+        return defaultFile();
+      }
+
+      return done(null, `${settings.assets.endpoint}/${dist}${data[asset]}`);
+    });
+  });
 };
