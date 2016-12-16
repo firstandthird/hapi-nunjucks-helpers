c127a29bc0b27a250be3ceccc69970138464d540
Chris <orthagonal1@gmail.com>
2016-12-16 16:36:15 -0600

    use clientkit-assets to load assets

--- helpers/asset.js
+++ helpers/asset.js
@@ -1,52 +1,7 @@
 'use strict';
-
-const fs = require('fs');
+const AssetMap = require('clientkit-assets');
 
 module.exports = function(server, asset, done) {
-  const settings = this.options.assets || {};
-  const dist = settings.dist || '';
-  const mappingFile = settings.mappingFile;
-  const endpoint = settings.endpoint || '';
-  const realm = server.realm.plugins['hapi-nunjucks-helpers'];
-
-  if (!settings.cache) {
-    realm.assetMap = false;
-  }
-
-  const defaultFile = () => done(null, `${endpoint}/${dist}${asset}`);
-  const mappedFile = () => {
-    const data = realm.assetMap;
-
-    if (!data) {
-      return defaultFile();
-    }
-
-    return done(null, `${endpoint}/${dist}${data[asset]}`);
-  };
-
-  if (!mappingFile) {
-    return defaultFile();
-  }
-
-  if (realm.assetMap) {
-    return mappedFile();
-  }
-
-  fs.access(mappingFile, fs.constants.R_OK, err => {
-    if (err) {
-      server.log(['nunjucks-helpers', 'assets'], 'mapping file not found, skipping.');
-      return defaultFile();
-    }
-
-    fs.readFile(mappingFile, (err2, data) => {
-      if (err2) {
-        server.log(['nunjucks-helpers', 'assets'], err2);
-        realm.assetMap = false;
-      } else {
-        realm.assetMap = JSON.parse(data);
-      }
-
-      mappedFile();
-    });
-  });
+  const assetMap = new AssetMap(this.options.assets);
+  assetMap.lookupAsset(asset, done);
 };
