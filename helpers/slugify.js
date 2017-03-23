7bd1c7b8df4f4ea99b0c02ccee52ae8e22bbedf2
Newton Koumantzelis <chrisohpedia@gmail.com>
2017-03-22 21:45:10 -0700

    Create: slugify helper

---
+++ helpers/slugify.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function(server, slug, done) {
+  const output = slug.toString().toLowerCase()
+      .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+      .replace(/^-+/, '')             // Trim - from start of text
+      .replace(/-+$/, '');
+
+  done(null, output);
+};
