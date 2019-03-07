474b803a74e17029963c3a96b05ee0cf8bf3146a
Antonio Laguna <sombragriselros@gmail.com>
2019-03-07 10:16:15 +0100

    Avoiding critical error on copy block due to undefined copy (#14)

--- helpers/copyBlock.js
+++ helpers/copyBlock.js
@@ -4,10 +4,15 @@ module.exports = function(copy, cls, done) {
   if (typeof copy === 'string') {
     copy = [copy];
   }
+
   if (typeof cls === 'function') {
     done = cls;
     cls = '';
   }
-  const out = copy.map((c) => `<p ${cls ? `class="${cls}"` : ''}>${c}</p>`);
-  done(null, out.join(''));
+
+  const out = Array.isArray(copy) ?
+    copy.map((c) => `<p${cls ? ` class="${cls}"` : ''}>${c}</p>`).join('') :
+    '';
+
+  done(null, out);
 };
